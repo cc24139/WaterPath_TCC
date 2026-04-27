767796ae8041bc22e56b8e6554d9e52507bb2178
DanielHSS <168001155+danielfofdi@users.noreply.github.com>
2026-04-27 12:31:16 -0300

    Header Responsivo
    Agora o header é responsivo.

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/components/ui/Logo";
+import { MdMenu, MdOutlineOpenInNew } from 'react-icons/md';
 
 interface HeaderProps {
     variant?: 'default' | 'dark';
@@ -9,21 +10,28 @@ export function Header({ variant = 'default' }: HeaderProps) {
     const text = variant === 'dark' ? 'text-[#FFFFFF]' : 'text-text-primary';
     
     return (
-        <nav className={`flex flex-row justify-between items-center px-6 w-full h-16 shadow-[0_8px_15px_rgba(23,166,191,0.25)] ${background}`}>
-            <div className="flex flex-row justify-between items-center gap-1">
-                <Logo className="h-10 w-auto"/>
-                <h1 className="font-bold text-primary text-[22px] font-heading">Water Path</h1>
+        <nav className={`flex flex-row justify-around gap-5 sm:justify-between items-center px-1 sm:px-6 w-full h-16 shadow-[0_8px_15px_rgba(23,166,191,0.25)] ${background} `}>
+            
+            <div className="flex flex-row justify-between gap-2">
+                <button className="sm:hidden">
+                    <MdMenu className="h-6 w-auto text-primary" />
+                </button>
+                
+                <div className="flex flex-row justify-between items-center gap-1">
+                    <Logo className="h-7 sm:h-7 md:h-10 w-auto"/>
+                    <h1 className="font-bold text-primary text-[18px] font-heading">Water Path</h1>
+                </div>
             </div>
 
-            <ul className="flex flex-row justify-between items-center gap-4 list-none">
-                <li className={`text-[16px] ${text} font-light`}>Sobre nós</li>
-                <li className={`text-[16px] ${text} font-light`}>Minhas Análises</li>
-                <li className={`text-[16px] ${text} font-light`}>Metodologias</li>
+            <ul className="hidden sm:flex flex-row justify-between items-center sm:gap-1 md:gap-4 list-none">
+                <li className={`md:text-[16px] sm:text-[8px] ${text} font-light`}>Sobre nós</li>
+                <li className={`md:text-[16px] sm:text-[8px] ${text} font-light`}>Minhas Análises</li>
+                <li className={`md:text-[16px] sm:text-[8px] ${text} font-light`}>Metodologias</li>
             </ul> 
             
-            <div className="flex flex-row justify-between items-center gap-2">
-                <h1 className={`text-[16px] font-semibold text-contrast`}>Criar Conta</h1>
-                <button className="px-4 py-1 rounded-lg bg-contrast text-white text-[16px] font-semibold shadow-[0_4px_8px_rgba(0,0,0,0.25)]">Comece agora</button>
+            <div className="flex flex-row justify-between items-center gap-0.5 md:gap-2">
+                <h1 className={`md:text-[16px] sm:text-[8px] text-[10px] font-semibold text-contrast`}>Criar Conta</h1>
+                <button className="px-4 py-1 rounded-lg bg-contrast text-white text-[11px] sm:text-[12px] font-semibold shadow-[0_4px_8px_rgba(0,0,0,0.25)]">Comece agora</button>
             </div>
                 
         </nav>
