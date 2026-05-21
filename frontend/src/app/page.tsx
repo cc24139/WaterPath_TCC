84803d27a6bf4335ec741128fba7c070e9868a93
cc24139 <lf44654@gmail.com>
2026-05-21 12:05:40 -0300

    Inico da tela de login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,21 +5,9 @@ import { RiverListToolBar } from "@/features/search/components/RiverListToolBar"
 import { RiverConditionCards } from "@/features/river-analysis/components/RiverConditionCards";
 import { RiverInsightCards } from "@/features/river-analysis/components/RiverInsightCards";
 import { SideBar} from "@/components/layout/SideBar";
-
+import LoginPage from "./loginPage";
 export default function Home() {
   return (
-    <>
-    <div className="hidden">
-
-    <InputField placeholder="Informe seu nome de usuário" />
-    <Button text="Entrar" variant="primary" />
-    <Button text="Cancelar" variant="secondary" />
-    <RiverListToolBar />
-    <RiverConditionCards />
-    <RiverInsightCards />
-    </div>
-    
-    <SideBar />
-    </>
+      <LoginPage />
   );
 }
