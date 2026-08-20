4d6e5ab5c60df226f7fead84256b163d8ee58182
cc24139 <lf44654@gmail.com>
2026-08-20 10:21:49 -0300

    correção do envio de email via render

--- back-end/api/obj/Debug/net10.0/back-end.AssemblyInfo.cs
+++ back-end/api/obj/Debug/net10.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d188539388c29eea9922c1d7dfbffb7e3bdac5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7b29b849f07347985e404a008ce224a696df39")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
